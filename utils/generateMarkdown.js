c255ee2765d5d88f0a39f1a842713467291ccf55
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-08-19 18:10:27 -0400

    adding code to render github license info

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== ""){
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }else{
+    return;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,12 +14,22 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license !== ""){
+    return `         ## License
+
+    This project is licensed under the ${license} license`
+  }else{
+    return;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  licenseSection = renderLicenseSection(data.license);
+  licenseBadge = renderLicenseBadge(data.license);
   return `# ${data.title}
-          ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+          ${licenseBadge}
 
           ## Description
 
@@ -45,9 +61,7 @@ function generateMarkdown(data) {
 
           ${data.usage}
 
-          ## License
-
-          This project is licensed under the ${data.license} license
+          ${licenseSection}
 
           ## Contributing
 
